13d3a8aa6a4d04b19cfdff6f63ab25d67b21f208
ehuang47 <44561409+ehuang47@users.noreply.github.com>
2023-06-29 21:48:18 -0400

    Initial Commit

---
+++ Noting/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Noting
+//
+//  Created by Ethan Huang on 6/29/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
